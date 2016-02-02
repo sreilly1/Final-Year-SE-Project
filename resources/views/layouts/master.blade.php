1fd1cfd6e24b3aedfbc3eb546641e96503a8e83e
=sreilly1 <samreilly100@hotmail.co.uk>
2016-02-02 17:10:24 +0000

    blade master layout file now has reference to jQuery 2.2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,6 +13,7 @@
         </div>
 
         <!-- Place Javascript file references here -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         @yield('javascript')
 
     </body>
