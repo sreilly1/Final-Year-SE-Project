fe73911fae4011ac99c52deb3a1ef3c18526e2d9
=sreilly1 <samreilly100@hotmail.co.uk>
2016-02-04 15:19:05 +0000

    added a migration to create the user_types table

---
+++ database/migrations/2016_02_04_150407_create_user_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_Types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('user_type', ['phd_Student', 'lecturer', 'admin']);
+            $table->timestamps();
+            
+        });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
