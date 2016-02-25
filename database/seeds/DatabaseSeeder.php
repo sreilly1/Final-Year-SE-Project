94771596d296b84d49a3e888d1dbf7f9e00b72b2
=sreilly1 <samreilly100@hotmail.co.uk>
2016-02-25 14:32:16 +0000

    added database seeder now calls the seeder for modules table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->call(LecturersTableSeeder::class);
         $this->call(ModulesTableSeeder::class);
     }
 }
