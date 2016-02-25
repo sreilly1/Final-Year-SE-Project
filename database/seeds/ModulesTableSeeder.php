167aae81dbfe841b97c2536738b72e582d48e519
=sreilly1 <samreilly100@hotmail.co.uk>
2016-02-25 14:34:17 +0000

    added modules table seeder

--- database/seeds/ModulesTableSeeder.php
+++ database/seeds/ModulesTableSeeder.php
@@ -13,12 +13,18 @@ class ModulesTableSeeder extends Seeder
      */
     public function run()
     {
-        //Read in a csv file containing data(module code, module name, module leader on the currently running modules in the school of Computer Science and Informatics)
+        //Read in a csv file containing data(module code, module name, module leader on the currently running modules 
+        //in the school of Computer Science and Informatics) from the _data directory
         $modulesCSVFile = new CsvFile(__DIR__ . '/_data/modules.csv');
 
-        //Generate a record using data from each row/line of the CSV file where each row is in the format: module code, module name, module leader
+        //Generate a record using data from each line of the CSV file where each row is in the format: 
+        //module code, module name, module leader, 
 		foreach($modulesCSVFile as $currentRow) {
-            Module::create(['module_code' => $currentRow[0], 'module_name' => $currentRow[1]]);
+            Module::create([
+                'module_code' => trim($currentRow[0]), 
+                'module_name' => trim($currentRow[1]),
+                'module leader' => '1';
+                ]);
         }
     }
 }
