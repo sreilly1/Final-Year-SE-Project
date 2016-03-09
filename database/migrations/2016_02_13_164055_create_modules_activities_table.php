343bfe5cd1bef7ccb1b5b236db5d2b7a5b0285f7
=sreilly1 <samreilly100@hotmail.co.uk>
2016-03-09 10:22:53 +0000

    added migration for Fahad's modified version of the modules_activities table

--- database/migrations/2016_02_13_164055_create_modules_activities_table.php
+++ database/migrations/2016_02_13_164055_create_modules_activities_table.php
@@ -14,17 +14,14 @@ class CreateModulesActivitiesTable extends Migration
     {
         Schema::create('modules_activities', function(Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
             $table->enum('role_type',['Demonstrator', 'Teaching']);
             $table->integer('module_id')->unsigned();
             $table->foreign('module_id')->references('id')->on('modules');
             $table->integer('staff_1')->unsigned();
             $table->foreign('staff_1')->references('id')->on('lecturers');
-            $table->dateTime('activity_date');
-            $table->string('Weeks');
-            $table->integer('from_hour');
-            $table->enum('am_pm',['AM', 'PM']);
-            $table->integer('to_hour');
-            $table->enum('am_pm_2',['AM', 'PM']);
+            $table->date('activity_date');
+            $table->time('activity_time');
             $table->integer('quant_ppl_needed');
             $table->string('location');
             $table->timestamps();
