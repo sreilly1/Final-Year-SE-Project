89d02505efd1897bd55b20eb533c8cb72c944ad5
=sreilly1 <samreilly100@hotmail.co.uk>
2016-03-27 13:47:59 +0100

    added Session model and defined the one-to-many relationship between it and an Activity (each session relates to one activity and each activity can have many sessions

--- app/Activity.php
+++ app/Activity.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Session;
 
 class Activity extends Model
 {
     protected $table = 'activities';
+
+    /**
+     * Get the sessions for the supprt activity(lab, tutorial, etc).
+     */
+    public function sessions() {
+
+    	//use some information from here (one to many relationship between Session and Activity):
+    	//https://laravel.com/docs/master/eloquent-relationships#one-to-many
+    	return $this->hasMany('App\Session');
+    }
 }
--- app/Session.php
+++ app/Session.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
-    //
+	/**
+     * Get the support activity that owns the session.
+     */
+    public function activity () {
+
+    	//use some information from here (one to many relationship between Session and Activity):
+    	//https://laravel.com/docs/master/eloquent-relationships#one-to-many
+    	return $this->belongsTo('App\Activity');
+    }
 }
