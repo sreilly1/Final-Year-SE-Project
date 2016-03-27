6020ffe1ae4f50f232cf981458dacf6d3da61b8d
=sreilly1 <samreilly100@hotmail.co.uk>
2016-03-27 17:18:26 +0100

    defined that an instance of an activity belongs to an instance of a module

--- app/Activity.php
+++ app/Activity.php
@@ -9,7 +9,7 @@ class Activity extends Model
     protected $table = 'activities';
 
     /**
-     * Get the sessions for the supprt activity(lab, tutorial, etc).
+     * Get the sessions for the support activity(lab, tutorial, etc).
      */
     public function sessions() {
 
@@ -23,5 +23,12 @@ class Activity extends Model
      */
    	public function activityRequests() {
    		return $this->hasMany('App\ActivityRequest');
+   	}
+
+   	/**
+     * Get the module that owns the support activity(lab, tutorial, etc).
+     */
+   	public function module() {
+   		return $this->belongsTo('App\Module');
    	}
 }
