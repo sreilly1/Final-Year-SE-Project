f37aa5b67f43ad706b8621ead10cecec5b6c0caf
=sreilly1 <samreilly100@hotmail.co.uk>
2016-03-28 22:27:53 +0100

    removed some empty lines from Activity seeder file and took care of some of the comments

--- database/seeds/ActivitiesSeeder.php
+++ database/seeds/ActivitiesSeeder.php
@@ -5,9 +5,6 @@ use Keboola\Csv\CsvFile;
 use App\Module;
 use App\Activity;
 
-
-use Symfony\Component\Console\Output\ConsoleOutput;
-
 class ActivitiesSeeder extends Seeder
 {
     /**
@@ -23,16 +20,10 @@ class ActivitiesSeeder extends Seeder
         //Read in a csv file containing data on some of the activities in the COMSC department 
         $activtiesCSVFile = new CsvFile(__DIR__ . '/_data/Activities.csv');
 
-        //get the 
-        // $output = new ConsoleOutput();
-        // $modules = Module::all();
-        // $output->writeln((string)$modules);
-
         //Generate a record using data from each line of the CSV file where each row is in the format: 
         //TODO:put the format here
         //'id' value for each module in the 'modules' table specified in the CSV file, as the module seeder 
         //(and all other seeders including this one) will only be run a single time when it has been put into production
-
 
         foreach($activtiesCSVFile as $currentRow) {
 
@@ -40,12 +31,11 @@ class ActivitiesSeeder extends Seeder
             //of the CSV file that is currently being processed. The first method returns only the first match
             //as the modules in the database should have unique module codes anyway.
             $module = Module::where('module_code','=', (string)$currentRow[2])->first();
-            $output = new ConsoleOutput();
-            $output->writeln((string) $module);
 
-            //access the 'id' field (primary key) of module received by the previous query
-            //$moduleId = $module->id;
+            //obtain the 'ID' (primary key) of the module that matches the above 'where' query
+            $moduleID = $module->id;
 
+            
             $moduleActivity = Activity::create([
             	'title' => trim($currentRow[0]),
             	'role_type' => trim($currentRow[1]),
@@ -58,9 +48,5 @@ class ActivitiesSeeder extends Seeder
         }
     }
 }
-
-
 //f0rmat in csv file: title   role type   m0dule  staff 1 quant ppl needed    cl0sing date f0r applicati0ns (in yyyy-dd-mm f0rmat/american)    kn0wledge required
-
-
 //T0D0:Make this wrk with inductin activities (there is a csv file f0r this)
