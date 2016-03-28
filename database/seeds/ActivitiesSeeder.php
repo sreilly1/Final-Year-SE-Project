67763e888187703813c5eea60fdd42c720d46647
=sreilly1 <samreilly100@hotmail.co.uk>
2016-03-28 21:46:43 +0100

    now seed data for support activities correctly and fixed errr where one module was missing from CSV file

--- database/seeds/ActivitiesSeeder.php
+++ database/seeds/ActivitiesSeeder.php
@@ -5,6 +5,9 @@ use Keboola\Csv\CsvFile;
 use App\Module;
 use App\Activity;
 
+
+use Symfony\Component\Console\Output\ConsoleOutput;
+
 class ActivitiesSeeder extends Seeder
 {
     /**
@@ -21,6 +24,9 @@ class ActivitiesSeeder extends Seeder
         $activtiesCSVFile = new CsvFile(__DIR__ . '/_data/Activities.csv');
 
         //get the 
+        // $output = new ConsoleOutput();
+        // $modules = Module::all();
+        // $output->writeln((string)$modules);
 
         //Generate a record using data from each line of the CSV file where each row is in the format: 
         //TODO:put the format here
@@ -30,15 +36,31 @@ class ActivitiesSeeder extends Seeder
 
         foreach($activtiesCSVFile as $currentRow) {
 
+            //get an object representing the first module whose module code matches the one in the row
+            //of the CSV file that is currently being processed. The first method returns only the first match
+            //as the modules in the database should have unique module codes anyway.
+            $module = Module::where('module_code','=', (string)$currentRow[2])->first();
+            $output = new ConsoleOutput();
+            $output->writeln((string) $module);
 
+            //access the 'id' field (primary key) of module received by the previous query
+            //$moduleId = $module->id;
 
             $moduleActivity = Activity::create([
             	'title' => trim($currentRow[0]),
             	'role_type' => trim($currentRow[1]),
-            	'module_id' => ($currentRow[2]),
-            	'quant_ppl_needed' => $currentRow[3],
-            	'knowledge_required' => $currentRow[4]
+            	'module_id' => $module->id,
+                'staff_1' => $currentRow[3],
+            	'quant_ppl_needed' => $currentRow[4],
+                'closing_date_for_applications' => $currentRow[5],
+            	'knowledge_required' => $currentRow[6]
             ]);
         }
     }
 }
+
+
+//f0rmat in csv file: title   role type   m0dule  staff 1 quant ppl needed    cl0sing date f0r applicati0ns (in yyyy-dd-mm f0rmat/american)    kn0wledge required
+
+
+//T0D0:Make this wrk with inductin activities (there is a csv file f0r this)
